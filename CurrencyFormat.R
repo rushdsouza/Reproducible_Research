892082b0023c250e10df2bac849a0cf1bc81937b
rushdsouza <rushdsouza@gmail.com>
2015-04-29 23:06:26 +0530

    replaced cbind with append
    replaced cbind with append

--- CurrencyFormat.R
+++ CurrencyFormat.R
@@ -1,13 +1,13 @@
 CurrencyFormat <-function (number,rounding=F) 
 {
     first <- TRUE
-    lut <- c( 1, 100, 1000, 1000000, 1000000000,1000000000000 )
-    pre <- c("", "H", "K", "M", "B", "T")
+    lut <- c( 1, 1000, 1000000, 1000000000,1000000000000 )
+    pre <- c("", "K", "M", "B", "T")
     if (length(number) > 1) {
         for (cnt in 1:length(number)){        
             ix <- findInterval(number[cnt], lut)
-            print(number[cnt])
-            print(cnt)
+#             print(number[cnt])
+#             print(cnt)
             if (ix != 0 | ix != 1){
                 if (rounding==T) {
                     sistring <- paste(round(number[cnt]/lut[ix]), pre[ix])
@@ -17,19 +17,41 @@ CurrencyFormat <-function (number,rounding=F)
                 }
                 if (first){
                     tnumber <- sistring
-                    fnumber <- tnumber 
+                    fnumber <- tnumber
                     first <- FALSE
                 }
                 else
-                    fnumber <- cbind(tnumber, fnumber)
+                    fnumber <- append(fnumber, sistring)
             }
             else {
                 sistring <- number[cnt]
+                if (first){
+                    tnumber <- sistring
+                    fnumber <- tnumber
+                    first <- FALSE
+                }
+                else
+                    fnumber <- append(fnumber, sistring)
             }
+#         print(paste("PMC Length : ", length(fnumber), sep=""))
         }
+#          print(paste("Final PMC Length", length(fnumber), class(fnumber), sep=" : "))        
         return(fnumber)
     }
-    else
-        return(number)
-    
+    else{
+        ix <- findInterval(number, lut)
+        if (ix != 0 | ix != 1){
+            if (rounding==T) {
+                sistring <- paste(round(number/lut[ix]), pre[ix])
+            }
+            else {
+                sistring <- paste(signif(number/lut[ix],digits=5), pre[ix])
+            }
+#             print("format 1 number")
+            return(sistring)
+        }    
+        else
+#             print("no format")
+            return(number)
+    }
 }
